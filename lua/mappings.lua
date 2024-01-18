019ff8343f487c3ca81e57d225c769aade1d7e1c
icaro-freire <icarofreire7@gmail.com>
2024-01-18 11:50:44 -0300

    <mod> mudei o atalho para visualização de pdf. Troquei o Zathura pelo MuPDF

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,7 +2,7 @@
 -- mapeamentos para LaTeX
 vim.keymap.set('n', '<leader>b', ':! arara -L pt-BR main.tex <cr>')
 vim.keymap.set('n', '<leader>v', ':! arara -L pt-BR -v main.tex <cr>')
-vim.keymap.set('n', '<leader>z', ':! zathura *.pdf <cr>')
+vim.keymap.set('n', '<leader>z', ':! mupdf *.pdf <cr>')
 
 vim.keymap.set('n', 'bb', ':! arara -L pt-BR %:p <cr>')
 
